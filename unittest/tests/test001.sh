5ae1fc1edd4c26fada51cd72e97339b42e7e07e9
rolf007 <rolf@asmund.dk>
2017-02-25 16:52:19 +0100

    Split unittest for diff and cursor from 'playing with split'

--- unittest/tests/test001.sh
+++ unittest/tests/test001.sh
@@ -1,55 +1,8 @@
 source "${BASH_SOURCE%/*}"/../setup.sh
 source $ROOT/"${BASH_SOURCE%/*}"/../inject.sh
-
-cat >>$vimtestdir/.vimrc <<EOL
-EOL
+#Test diff and cursor
 
 cat >>$vimtestdir/test.vim <<EOL
-
-let s:cur_debug_pid = 0
-
-function! s:DebugMove(delta)
-	let g:test_players[s:cur_debug_pid].range[1] += a:delta
-	let g:test_players[s:cur_debug_pid].range[3] += a:delta
-	call SendCursor(s:cur_debug_pid)
-endfunction
-
-function! SendCursor(from_pid)
-	call SendToDut('cursor', a:from_pid, [g:test_players[a:from_pid].file, g:test_players[a:from_pid].mode] + g:test_players[a:from_pid].range)
-endfunction
-
-function! s:DebugConnect()
-	let s:cur_debug_pid = CreateTestPlayer()
-	call s:DebugChange(s:cur_debug_pid-1000000)
-	call SendToDut('hello', s:cur_debug_pid, [])
-	call SendToDut('iam', s:cur_debug_pid, ["debug".s:cur_debug_pid])
-	call SendCursor(s:cur_debug_pid)
-	nnoremap <UP> :call <SID>DebugMove(-1)<CR>
-	nnoremap <DOWN> :call <SID>DebugMove(1)<CR>
-endfunction
-
-function! s:DebugChange(count)
-	if s:cur_debug_pid != 0 && a:count == 0
-		let s:cur_debug_pid = 0
-		nunmap <UP>
-		nunmap <DOWN>
-	elseif s:cur_debug_pid == 0 && a:count != 0
-		nnoremap <UP> :call <SID>DebugMove(-1)<CR>
-		nnoremap <DOWN> :call <SID>DebugMove(1)<CR>
-	endif
-	if a:count != 0
-		let s:cur_debug_pid = 1000000 + a:count
-	endif
-endfunction
-
-function! s:DebugDisconnect()
-endfunction
-
-execute "nnoremap <silent> mc :<C-U>call <SID>DebugConnect()<CR>"
-execute "nnoremap <silent> mm :<C-U>call <SID>DebugChange(v:count)<CR>"
-execute "nnoremap <silent> md :<C-U>call <SID>DebugDisconnect()<CR>"
-
-
 
 MultiplayerConnect
 
@@ -65,7 +18,8 @@ call assert_equal(ExpectedMsg('cursor', ['a.txt', 'n', 1, 1, 1, 1]), GetMsg(my_p
 call assert_equal(0, GetMsg(my_pid))
 
 call SendToDut("iam", my_pid, ['Tester'])
-call SendCursor(my_pid)
+call SendToDut('cursor', my_pid, ['a.txt', 'n', 1, 1, 1, 1])
+
 call SendToDut("diff", my_pid, ['a.txt', '1c1', '< ', '---', '> hello world'])
 call assert_equal(0, GetMsg(my_pid))
 call assert_equal(['hello world'], getline(1, '$'))
@@ -74,7 +28,7 @@ call SendToDut("diff", my_pid, ['a.txt', '1a2,3', "> \<TAB>12345\<TAB>123", '> 1
 call assert_equal(['hello world', "\<TAB>12345\<TAB>123", '123456789'], getline(1, '$'))
 
 let g:test_players[my_pid].range = ['6', '2', '6', '2']
-call SendCursor(my_pid)
+call SendToDut('cursor', my_pid, ['a.txt', 'n', '6', '2', '6', '2'])
 let m = getmatches()
 call assert_equal(1, len(m))
 call assert_equal('MPCol2', m[0].group)
---
+++ unittest/tests/test008.sh
@@ -0,0 +1,97 @@
+source "${BASH_SOURCE%/*}"/../setup.sh
+source $ROOT/"${BASH_SOURCE%/*}"/../inject.sh
+#Playig around with split
+
+cat >>$vimtestdir/.vimrc <<EOL
+EOL
+
+cat >>$vimtestdir/test.vim <<EOL
+
+let s:cur_debug_pid = 0
+
+function! s:DebugMove(delta)
+	if s:cur_debug_pid == 0
+		return
+	endif
+	let g:test_players[s:cur_debug_pid].range[1] += a:delta[1]
+	let g:test_players[s:cur_debug_pid].range[3] += a:delta[1]
+	let g:test_players[s:cur_debug_pid].range[0] += a:delta[0]
+	let g:test_players[s:cur_debug_pid].range[2] += a:delta[0]
+	call SendCursor(s:cur_debug_pid)
+endfunction
+
+function! SendCursor(from_pid)
+	call SendToDut('cursor', a:from_pid, [g:test_players[a:from_pid].file, g:test_players[a:from_pid].mode] + g:test_players[a:from_pid].range)
+endfunction
+
+function! s:DebugSetCur(pid)
+	if s:cur_debug_pid != 0
+		call SendToDut('iam', s:cur_debug_pid, ["debug".(s:cur_debug_pid-1000000)])
+	endif
+	let s:cur_debug_pid = a:pid
+	call SendToDut('iam', s:cur_debug_pid, ["*debug".(s:cur_debug_pid-1000000)."*"])
+endfunction
+
+function! s:DebugConnect()
+	let pid = CreateTestPlayer()
+	let g:test_players[pid].file = "a.txt"
+	let g:test_players[pid].mode = "n"
+	let g:test_players[pid].range = [1,1,1,1]
+	call SendToDut('hello', pid, [])
+	call <SID>DebugSetCur(pid)
+	call SendCursor(s:cur_debug_pid)
+endfunction
+
+
+function! s:DebugNext()
+	let prev = -1
+	for key in keys(g:test_players)
+		if prev == s:cur_debug_pid
+			call <SID>DebugSetCur(key)
+			return
+		endif
+		let prev = key
+	endfor
+endfunction
+
+function! s:DebugPrev()
+	let prev = -1
+	for key in keys(g:test_players)
+		if key == s:cur_debug_pid && prev != -1
+			call <SID>DebugSetCur(prev)
+			return
+		endif
+		let prev = key
+	endfor
+endfunction
+
+function! s:DebugDisconnect()
+endfunction
+
+nnoremap mc :<C-U>call <SID>DebugConnect()<CR>
+nnoremap md :<C-U>call <SID>DebugDisconnect()<CR>
+nnoremap mn :call <SID>DebugNext()<CR>
+nnoremap mp :call <SID>DebugPrev()<CR>
+nnoremap <C-Up> :call <SID>DebugMove([0,-1])<CR>
+nnoremap <C-Down> :call <SID>DebugMove([0,1])<CR>
+nnoremap <C-Left> :call <SID>DebugMove([-1,0])<CR>
+nnoremap <C-Right> :call <SID>DebugMove([1,0])<CR>
+
+execute("normal! i123\<CR>\<ESC>")
+execute("normal! i456\<CR>\<ESC>")
+execute("normal! i789\<CR>\<ESC>")
+execute("normal! i1011\<CR>\<ESC>")
+execute("normal! i1213\<CR>\<ESC>")
+execute("normal! i1415\<CR>\<ESC>")
+execute("normal! 3k")
+MultiplayerConnect
+
+EOL
+
+HOME=$vimtestdir vim -X a.txt
+
+popd > /dev/null
+source "${BASH_SOURCE%/*}"/../tear_down.sh
+exit 0
+
+vim:tw=78:ts=4:ft=vim:
